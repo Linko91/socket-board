29ea51207d9746e8e45aadb586558be7eacad457
Davide Di Modica <Linko@mbp-di-davide.ticonzero.locale>
2015-10-29 14:25:02 +0100

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', ['ngMaterial']);
 
 app.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default').dark();
+  $mdThemingProvider.theme('blue');
 });
 
 app.controller('appCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
